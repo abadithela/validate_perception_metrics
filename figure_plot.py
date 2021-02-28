303cd78f954d8c0fecb8171eeef9a7829f109d07
Apurva Badithela <apurvabadithela@Apurvas-MacBook-Pro.local>
2021-02-28 11:41:30 -0800

    push changes

--- figure_plot.py
+++ figure_plot.py
@@ -2,21 +2,25 @@ import matplotlib.pyplot as plt
 import numpy as np
 import json
 
-def probability_plot(INIT_V, P):
+def probability_plot(INIT_V, P, fig_name):
     fig, ax = plt.subplots()
     for k,v in INIT_V.items():
         probabilities = P[k]
         init_speed = INIT_V[k]
-        plt.plot(probabilities, init_speed, 'o--', label=f"V_{max}={k}")
+        plt.plot(init_speed, probabilities, 'o--', label=f"V={k}")
     leg = plt.legend(loc="best")
+    plt.xlabel("Initial speed")
+    plt.ylabel("Probability of satisfaction")
+    plt.xticks(np.arange(1,8,1))
+    plt.savefig(fig_name, format='png', dpi=1200)
     plt.show()
     
-fname_v = "type_1_init_v_20210228-045604_.json"
-fname_p = "type_1_prob_20210228-045604_.json"
+fname_v = "type_1_init_v_20210228-184123_.json"
+fname_p = "type_1_prob_20210228-184123_.json"
 
 with open(fname_v) as fv:
     INIT_V = json.load(fv)
 with open(fname_p) as fp:
     P = json.load(fp)
-
-probability_plot(INIT_V, P)
+fig_name = "type_1_init_v_20210228-184123_.png"
+probability_plot(INIT_V, P, fig_name)
